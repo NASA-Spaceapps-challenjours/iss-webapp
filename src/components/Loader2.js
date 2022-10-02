21fbe1cbafd686832c54386556e4663e8c93cda5
JACOB SINGER <jacobbradleysinger@gmail.com>
2022-10-01 20:19:52 -0500

    update

--- src/components/Loader2.js
+++ src/components/Loader2.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Lottie from "react-lottie";
 
 import * as location from "../49242-world.json";
