9604c848bef29bfff66e06a70c91fa058d3939e8
Van <bao_van1995@yahoo.com>
2022-10-02 01:26:01 -0500

    added other mother file uwu

---
+++ src/pages/mommy.tsx
@@ -0,0 +1,82 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+const mommy: NextPage = () => {
+  return (
+        <>
+            <Head>
+                <title>your other mother.</title>
+                <meta name="description" content="your other mother." />
+                <link rel="icon" href="/favicon.ico" />
+                
+            </Head>
+            <main className="">
+
+                    <h1>About Us</h1>
+                <h2>track the iss</h2>
+                <p>What's even better than tracking the Internation Space Station? It's tracking it in 3D. The purpose of challenjours-track-iss is to track the ISS past, current, and future location while providing a fun, interactive, user-friendly interface. </p>
+
+                <h2>User Guide</h2>
+
+                <p>rawr</p>
+                <h2>The Challenjours</h2>
+
+                <div className="row">
+                    <div class="column ">
+                        <img src="" alt="Rebecca Soza"></img>
+                    </div>
+                    <div className="column" style="width:100%">
+                         <img src="" alt="Bao-Van Nguyen"></img>
+                         </div>
+                    <div className="column" style="width:100%">
+                        <img src="" alt="Jacob Singers"></img>
+                        </div>
+                    <div className="column"style="width:100%">
+                        <img src="" alt="Adrian Yip"></img>
+                        </div>
+                    <div className="column" style="width:100%">
+                        <img src="" alt="Benjamin Serrano"></img>
+                        </div>
+                </div>
+                <div className="row">
+                    <div className="column">
+                        <p>Rebecca Soza</p>
+                    </div>
+                    <div className="column">
+                        <p>Bao-Van Nguyen</p>
+                    </div>
+                    <div className="column">
+                        <p>Jacob Singers</p>
+                    </div>
+                    <div className="column">
+                        <p>Adrian Yip</p>
+                    </div>
+                    <div className="column">
+                        <p>Benjamin Serrano</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="column">
+                        <p>About Rebecca</p>
+                    </div>
+                    <div className="column">
+                        <p>About Bao-Van</p>
+                    </div>
+                    <div className="column">
+                        <p>About Jacob</p>
+                    </div>
+                    <div className="column">
+                        <p>About Adrian</p>
+                    </div>
+                    <div className="column">
+                        <p>About Benjamin</p>
+                    </div>
+                </div>
+                <h2>List of Attributions</h2>
+                
+                <p>ISS API</p>
+            </main>
+        </>
+  );
+}
+
+export default mommy;
