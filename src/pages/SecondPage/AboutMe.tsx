b8ba4993115cf4873a4885f27a5546e39d718eae
JACOB SINGER <jacobbradleysinger@gmail.com>
2022-10-02 05:59:12 -0500

    update

--- src/pages/SecondPage/AboutMe.tsx
+++ src/pages/SecondPage/AboutMe.tsx
@@ -6,7 +6,7 @@ function ISS() {
     <div className="bg-black">
       <header className="App-header">
         <Parallax
-          pages={2}
+          pages={3}
           style={{ top: "0", left: "0", background: "black" }}
         >
           <ParallaxLayer
@@ -92,10 +92,101 @@ function ISS() {
               fontStyle: "italic",
             }}
           ></ParallaxLayer>
+          <div className="flex justify-center">
+            <ParallaxLayer
+              offset={1.0}
+              speed={0.5}
+              style={{
+                display: "flex-column",
+                justifyContent: "",
+                alignItems: "center",
+                color: "white",
+                fontStyle: "italic",
+                height: 800,
+                width: 350,
+                marginLeft: 850,
+                marginTop: 100
+              }}
+            >
+              <div className='space-y-1'>
+                <h1 className='text-7xl'>About Us</h1>
+                <h2 className='px-14 text-2xl'>-----track the iss-----</h2>
+                <p>What's even better than tracking the Internation Space Station?</p>
+                <br />
+                <h2 className='px-12 text-2xl'>-----Challenjours-----</h2>
+
+                <div className="justify-content">
+                  <div className="column px-12">
+                    <img src="" alt="Rebecca Soza"></img>
+                  </div>
+                  <div className="column px-12">
+                    <img src="" alt="Bao-Van Nguyen"></img>
+                  </div>
+                  <div className="column px-12">
+                    <img src="" alt="Jacob Singers"></img>
+                  </div>
+                  <div className="column px-12">
+                    <img src="" alt="Adrian Yip"></img>
+                  </div>
+                  <div className="column px-12">
+                    <img src="" alt="Benjamin Serrano"></img>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="column px-12">
+                    <p>Rebecca Soza</p>
+                  </div>
+                  <div className="column px-12">
+                    <p>Bao-Van Nguyen</p>
+                  </div>
+                  <div className="column px-12">
+                    <p>Jacob Singers</p>
+                  </div>
+                  <div className="column px-12">
+                    <p>Adrian Yip</p>
+                  </div>
+                  <div className="column px-12">
+                    <p>Benjamin Serrano</p>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="column px-12">
+                    <p>About Rebecca</p>
+                  </div>
+                  <div className="column px-12">
+                    <p>About Bao-Van</p>
+                  </div>
+                  <div className="column px-12">
+                    <p>About Jacob</p>
+                  </div>
+                  <div className="column px-12">
+                    <p>About Adrian</p>
+                  </div>
+                  <div className="column px-12">
+                    <p>About Benjamin</p>
+                  </div>
+                </div>
+                <h2 className='px-14 text-2xl'>-----Attributions-----</h2>
+                <a href="https://www.npmjs.com/package/react-lottie">React Lottie</a>
+                <br />
+                <a href="https://react-spring.dev/components/parallax">React Spring Paralax</a>
+                <br />
+                <a href="https://worldwind.arc.nasa.gov/autodocs/WebWorldWind/"> WorldWind </a>
+                <br />
+                <a href="https://spotthestation.nasa.gov/"> Spot the Station </a>
+                <br />
+                <a href="https://github.com/joshuaferrara/go-satellite"> Go Satellite </a>
+                <br />
+                <a href="https://github.com/gin-gonic/gin"> Gin Gonic</a>
+                <br />
+                <a href="https://github.com/gin-gonic/contrib"> Gin Gonic Contrib </a>
+              </div>
+            </ParallaxLayer >
+          </div >
 
           <ParallaxLayer
-            offset={1.25}
-            speed={0.5}
+            offset={1.0}
+            speed={3.5}
             style={{
               display: "flex",
               justifyContent: "center",
@@ -104,11 +195,30 @@ function ISS() {
               fontStyle: "italic",
             }}
           >
-            Hello,
           </ParallaxLayer>
-        </Parallax>
-      </header>
-    </div>
+
+          <ParallaxLayer
+            offset={2.0}
+            speed={0.5}
+            style={{
+              display: "flex-column",
+              justifyContent: "",
+              alignItems: "center",
+              color: "white",
+              fontStyle: "italic",
+              height: 800,
+              width: 350,
+              marginLeft: 850,
+              marginTop: 100
+            }}
+          >
+            <div className='space-y-1'>
+              <h2 className='px-14 text-2xl'>-----User Guide-----</h2>
+            </div>
+          </ParallaxLayer>
+        </Parallax >
+      </header >
+    </div >
   );
 }
 
